3840c65decb0bb97fba68098580a9321b535cc36
Babajide Ibiayo <babajideibiayo@yahoo.com>
2024-12-28 11:23:10 +0100

    implemented update of data on server

--- server.js
+++ server.js
@@ -33,9 +33,35 @@ app.post("/add-data", async (req, res) => {
     res.status(500).json({ success: false, message: "Error updating data" })
   }
 })
-app.post("/update-data", async () => {
+app.post("/update-data", async (req, res) => {
   // Todo: Implement
-  console.log("Connected to server for update")
+  try {
+    const dataPath = path.join(__dirname, "data.json")
+    const data = await fs.readFile(dataPath, "utf8")
+    const jsonData = JSON.parse(data)
+    const index = jsonData.findIndex((item) => item.id === req.body[0].id)
+    if (index === -1) {
+      console.log("Data not found")
+      return res.status(404).json({ success: false, message: "Data not found" })
+    } else {
+      console.log("Data found")
+      if (req.body[0].index === 0) {
+        jsonData[index]["name"] = req.body[0].value
+      } else if (req.body[0].index === 1) {
+        jsonData["email"] = req.body[0].value
+      } else if (req.body[0].index === 2) {
+        jsonData["status"] = req.body[0].value
+      } else {
+        console.log("Invalid index")
+      }
+    }
+    console.log(jsonData)
+    await fs.writeFile(dataPath, JSON.stringify(jsonData, null, 2))
+    res.json({ success: true, message: "Data updated successfully" })
+  } catch (error) {
+    console.error("Error updating data:", error)
+    res.status(500).json({ success: false, message: "Error updating data" })
+  }
 })
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
